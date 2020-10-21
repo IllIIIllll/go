c98d50016bbeed408eeb432d0f74eadabfc566dd
JISUNG PARK <llllllllll@kakao.com>
2020-10-21 18:44:27 +0900

    feat: Add Agent class
    기본 에이전트 정의

---
+++ dlgo/agent/base.py
@@ -0,0 +1,10 @@
+# © 2020 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# MIT License
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def select_move(self, game_state):
+        raise NotImplementedError()
