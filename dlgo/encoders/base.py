4e635e7bfd85e81a6b9d1b7125c9c5f22040cb5b
JISUNG PARK <llllllllll@kakao.com>
2020-10-23 02:56:31 +0900

    feat: Add get_encoder_by_name function
    변환기 이름을 이용하여 변환기 생성

--- dlgo/encoders/base.py
+++ dlgo/encoders/base.py
@@ -2,6 +2,8 @@
 # <llllllllll@kakao.com>
 # MIT License
 
+import importlib
+
 class Encoder:
     # 변환기 이름 로깅
     def name(self):
@@ -26,3 +28,11 @@ class Encoder:
     # 변환된 바둑판의 모양
     def shape(self):
         raise NotImplementedError
+
+# 변환기 이름을 사용하여 변환기 생성
+def get_encoder_by_name(name, board_size):
+    if isinstance(board_size, int):
+        board_size = (board_size, board_size)
+    module = importlib.import_module('dlgo.encoders.' + name)
+    constructor = getattr(module, 'create')
+    return constructor(board_size)
