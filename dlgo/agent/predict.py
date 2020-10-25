0cbf4d6fc6f56ece12e7b62ab3f0ccfadaf446ff
JISUNG PARK <llllllllll@kakao.com>
2020-10-26 04:13:05 +0900

    feat: Add DeepLearning agent
    심층 신경망을 이용한 수 예측 에이전트 구축

---
+++ dlgo/agent/predict.py
@@ -0,0 +1,69 @@
+# © 2020 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# MIT License
+
+import numpy as np
+
+from dlgo.agent.base import Agent
+from dlgo.agent.helpers import is_point_an_eye
+from dlgo import encoders
+from dlgo import goboard
+from dlgo import kerasutil
+
+class DeepLearningAgent(Agent):
+    def __init__(self, model, encoder):
+        Agent.__init__(self)
+        self.model = model
+        self.encoder = encoder
+
+    def predict(self, game_state):
+        encoded_state = self.encoder.encode(game_state)
+        input_tensor = np.array([encoded_state])
+        return self.model.predict(input_tensor)[0]
+
+    def select_move(self, game_state):
+        num_moves = self.encoder.board_width * self.encoder.board_height
+        move_probs = self.predict(game_state)
+
+        move_probs = move_probs ** 3
+        eps = 1e-6
+        # 예측값이 0이나 1에 가까워지는 것을 방지
+        move_probs = np.clip(move_probs, eps, 1 - eps)
+        # 재정규화
+        move_probs = move_probs / np.sum(move_probs)
+
+        # 수 리스트에 랭킹을 부여
+        candidates = np.arange(num_moves)
+        ranked_moves = np.random.choice(
+            candidates, num_moves, replace=False, p=move_probs
+        )
+        for point_idx in ranked_moves:
+            point = self.encoder.decode_point_index(point_idx)
+            if game_state.is_valid_move(goboard.Move.play(point)) and \
+                    not is_point_an_eye(
+                        game_state.board, point,
+                        game_state.next_player
+                    ):
+                return goboard.Move.play(point)
+        return goboard.Move.pass_turn()
+
+    # 에이전트 직렬화
+    def serialize(self, h5file):
+        h5file.create_group('encoder')
+        h5file['encoder'].attrs['name'] = self.encoder.name()
+        h5file['encoder'].attrs['board_width'] = self.encoder.board_width
+        h5file['encoder'].attrs['board_height'] = self.encoder.board_height
+        h5file.create_group('model')
+        kerasutil.save_model_to_hdf5_group(self.model, h5file['model'])
+
+# HDF5 파일에서 에이전트 역직렬화
+def load_prediction_agent(h5file):
+    model = kerasutil.load_model_from_hdf5_group(h5file['model'])
+    encoder_name = h5file['encoder'].attrs['name']
+    if not isinstance(encoder_name, str):
+        encoder_name = encoder_name.decode('ascii')
+    board_width = h5file['encoder'].attrs['board_width']
+    board_height = h5file['encoder'].attrs['board_height']
+    encoder = encoders.get_encoder_by_name(
+        encoder_name, (board_width, board_height))
+    return DeepLearningAgent(model, encoder)
