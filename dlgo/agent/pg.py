cbca6641a0e407bf6c917a93b730ba6557f8653c
JISUNG PARK <llllllllll@kakao.com>
2020-10-28 14:13:17 +0900

    feat: Merge ExperienceCollector and PolicyAgent

--- dlgo/agent/pg.py
+++ dlgo/agent/pg.py
@@ -24,20 +24,23 @@ class PolicyAgent(Agent):
 
     def serialize(self, h5file):
         h5file.create_group('encoder')
-        h5file['encoder'].attrs['name'] = self._encoder.name()
-        h5file['encoder'].attrs['board_width'] = self._encoder.board_width
-        h5file['encoder'].attrs['board_height'] = self._encoder.board_height
+        h5file['encoder'].attrs['name'] = self.encoder.name()
+        h5file['encoder'].attrs['board_width'] = self.encoder.board_width
+        h5file['encoder'].attrs['board_height'] = self.encoder.board_height
         h5file.create_group('model')
-        kerasutil.save_model_to_hdf5_group(self._model, h5file['model'])
+        kerasutil.save_model_to_hdf5_group(self.model, h5file['model'])
+
+    def set_collector(self, collector):
+        self.collector = collector
 
     def select_move(self, game_state):
-        board_tensor = self._encoder.encode(game_state)
+        board_tensor = self.encoder.encode(game_state)
         X = np.array([board_tensor])
-        move_probs = self._model.predict(X)[0]
+        move_probs = self.model.predict(X)[0]
 
         move_probs = clip_probs(move_probs)
 
-        num_moves = self._encoder.board_width * self._encoder.board_height
+        num_moves = self.encoder.board_width * self.encoder.board_height
         candidates = np.arange(num_moves)
         ranked_moves = np.random.choice(
             candidates,
@@ -46,7 +49,7 @@ class PolicyAgent(Agent):
             p=move_probs
         )
         for point_idx in ranked_moves:
-            point = self._encoder.decode_point_index(point_idx)
+            point = self.encoder.decode_point_index(point_idx)
             move = goboard.Move.play(point)
             is_valid = game_state.is_valid_move(move)
             is_an_eye = is_point_an_eye(
@@ -54,8 +57,12 @@ class PolicyAgent(Agent):
                 point,
                 game_state.next_player
             )
-            if is_valid and (not is_an_eye):
-                return goboard.Move.play(point)
+            if self.collector is not None:
+                self.collector.record_decision(
+                    state=board_tensor,
+                    action=point_idx
+                )
+            return goboard.Move.play(point)
         return goboard.Move.pass_turn()
 
 def load_policy_agent(h5file):
