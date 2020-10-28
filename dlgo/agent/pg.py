5301a4c2f99e0cccc509343d18fb319484f462ec
JISUNG PARK <llllllllll@kakao.com>
2020-10-28 15:25:08 +0900

    feat: Apply policy gradient learning
    경사하강법을 이용하여 정책 수정

--- dlgo/agent/pg.py
+++ dlgo/agent/pg.py
@@ -3,6 +3,7 @@
 # MIT License
 
 import numpy as np
+from tensorflow.keras.optimizers import SGD
 
 from dlgo.agent.base import Agent
 from dlgo import kerasutil
@@ -10,17 +11,25 @@ from dlgo import encoders
 from dlgo.agent.helpers import is_point_an_eye
 from dlgo import goboard
 
-def clip_probs(original_probs):
-    min_p = 1e-5
-    max_p = 1 - min_p
-    clipped_probs = np.clip(original_probs, min_p, max_p)
-    clipped_probs = clipped_probs / np.sum(clipped_probs)
-    return clipped_probs
+def normalize(x):
+    total = np.sum(x)
+    return x / total
+
+def prepare_experience_data(experience, board_width, board_height):
+    experience_size = experience.actions.shape[0]
+    target_vectors = np.zeros((experience_size, board_width * board_height))
+    for i in range(experience_size):
+        action = experience.actions[i]
+        reward = experience.rewards[i]
+        target_vectors[i][action] = reward
+    return target_vectors
 
 class PolicyAgent(Agent):
     def __init__(self, model, encoder):
         self.model = model
         self.encoder = encoder
+        self.collector = None
+        self.temperature = 0.0
 
     def serialize(self, h5file):
         h5file.create_group('encoder')
@@ -33,45 +42,67 @@ class PolicyAgent(Agent):
     def set_collector(self, collector):
         self.collector = collector
 
+    def set_temperature(self, temperature):
+        self.temperature = temperature
+
     def select_move(self, game_state):
+        num_moves = self.encoder.board_width * self.encoder.board_height
+
         board_tensor = self.encoder.encode(game_state)
         X = np.array([board_tensor])
-        move_probs = self.model.predict(X)[0]
 
-        move_probs = clip_probs(move_probs)
+        if np.random.random() < self.temperature:
+            move_probs = np.ones(num_moves) / num_moves
+        else:
+            move_probs = self.model.predict(X)[0]
 
-        num_moves = self.encoder.board_width * self.encoder.board_height
+        eps = 1e-5
+        move_probs = np.clip(move_probs, eps, 1 - eps)
+        move_probs = move_probs / np.sum(move_probs)
+
         candidates = np.arange(num_moves)
         ranked_moves = np.random.choice(
-            candidates,
-            num_moves,
-            replace=False,
-            p=move_probs
-        )
+            candidates, num_moves, replace=False, p=move_probs)
         for point_idx in ranked_moves:
             point = self.encoder.decode_point_index(point_idx)
-            move = goboard.Move.play(point)
-            is_valid = game_state.is_valid_move(move)
-            is_an_eye = is_point_an_eye(
-                game_state.board,
-                point,
-                game_state.next_player
-            )
-            if self.collector is not None:
-                self.collector.record_decision(
-                    state=board_tensor,
-                    action=point_idx
-                )
-            return goboard.Move.play(point)
+            if game_state.is_valid_move(goboard.Move.play(point)) and \
+                    not is_point_an_eye(game_state.board,
+                                        point,
+                                        game_state.next_player):
+                if self.collector is not None:
+                    self.collector.record_decision(
+                        state=board_tensor,
+                        action=point_idx
+                    )
+                return goboard.Move.play(point)
         return goboard.Move.pass_turn()
+
+    def train(self, experience, lr, clipnorm, batch_size):
+        self.model.compile(
+            loss='categorical_crossentropy',
+            optimizer=SGD(lr=lr, clipnorm=clipnorm)
+        )
+
+        target_vectors = prepare_experience_data(
+            experience,
+            self.encoder.board_width,
+            self.encoder.board_height
+        )
+
+        self.model.fit(
+            experience.states, target_vectors,
+            batch_size=batch_size,
+            epochs=1
+        )
 
 def load_policy_agent(h5file):
     model = kerasutil.load_model_from_hdf5_group(h5file['model'])
     encoder_name = h5file['encoder'].attrs['name']
+    if not isinstance(encoder_name, str):
+        encoder_name = encoder_name.decode('ascii')
     board_width = h5file['encoder'].attrs['board_width']
     board_height = h5file['encoder'].attrs['board_height']
     encoder = encoders.get_encoder_by_name(
         encoder_name,
-        (board_width, board_height)
-    )
+        (board_width, board_height))
     return PolicyAgent(model, encoder)
