287217518c86648e0ab5154f0a20a6ccc7e9262a
JISUNG PARK <llllllllll@kakao.com>
2020-11-04 05:23:48 +0900

    feat: Apply Actor-Critic Learning in experience module
    Actor-Critic 학습 적용
     - 어드밴티지(실제 결과와 예상 결과의 차이) 적용

--- dlgo/rl/experience.py
+++ dlgo/rl/experience.py
@@ -9,35 +9,34 @@ class ExperienceBuffer(object):
         self.states = states
         self.actions = actions
         self.rewards = rewards
+        self.advantages = advantages
 
     def serialize(self, h5file):
         h5file.create_group('experience')
         h5file['experience'].create_dataset('states', data=self.states)
         h5file['experience'].create_dataset('actions', data=self.actions)
         h5file['experience'].create_dataset('rewards', data=self.rewards)
-
-def load_experience(h5file):
-    return ExperienceBuffer(
-        states=np.array(h5file['experience']['states']),
-        actions=np.array(h5file['experience']['actions']),
-        rewards=np.array(h5file['experience']['rewards'])
-    )
+        h5file['experience'].create_dataset('advantages', data=self.advantages)
 
 class ExperienceCollector(object):
     def __init__(self):
         self.states = []
         self.actions = []
         self.rewards = []
+        self.advantages = []
         self._current_episode_states = []
         self._current_episode_actions = []
+        self._current_episode_estimated_values = []
 
     def begin_episode(self):
         self._current_episode_states = []
         self._current_episode_actions = []
+        self._current_episode_estimated_values = []
 
-    def record_decision(self, state, action):
+    def record_decision(self, state, action, estimated_value=0):
         self._current_episode_states.append(state)
         self._current_episode_actions.append(action)
+        self._current_episode_estimated_values.append(estimated_value)
 
     def complete_episode(self, reward):
         num_states = len(self._current_episode_states)
@@ -45,8 +44,13 @@ class ExperienceCollector(object):
         self.actions += self._current_episode_actions
         self.rewards += [reward for _ in range(num_states)]
 
+        for i in range(num_states):
+            advantage = reward - self._current_episode_estimated_values[i]
+            self.advantages.append(advantage)
+
         self._current_episode_states = []
         self._current_episode_actions = []
+        self._current_episode_estimated_values = []
 
     def to_buffer(self):
         return ExperienceBuffer(
@@ -54,3 +58,32 @@ class ExperienceCollector(object):
             actions=np.array(self.actions),
             rewards=np.array(self.rewards)
         )
+
+def load_experience(h5file):
+    return ExperienceBuffer(
+        states=np.array(h5file['experience']['states']),
+        actions=np.array(h5file['experience']['actions']),
+        rewards=np.array(h5file['experience']['rewards']),
+        advantages=np.array(h5file['experience']['advantages'])
+    )
+
+def combine_experience(collectors):
+    combined_states = np.concatenate(
+        [np.array(c.states) for c in collectors]
+    )
+    combined_actions = np.concatenate(
+        [np.array(c.actions) for c in collectors]
+    )
+    combined_rewards = np.concatenate(
+        [np.array(c.rewards) for c in collectors]
+    )
+    combined_advantages = np.concatenate([
+        np.array(c.advantages) for c in collectors]
+    )
+
+    return ExperienceBuffer(
+        combined_states,
+        combined_actions,
+        combined_rewards,
+        combined_advantages
+    )
