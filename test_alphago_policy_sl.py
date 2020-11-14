5113f80e9cb14feb65c89c96671e01bdc6fe66c3
JISUNG PARK <llllllllll@kakao.com>
2020-11-15 00:30:13 +0900

    test: Add alphago supervised learning model

--- test_alphago_policy_sl.py
+++ test_alphago_policy_sl.py
@@ -4,6 +4,7 @@
 
 from dlgo.data.processor import GoDataProcessor
 from dlgo.encoders.alphago import AlphaGoEncoder
+from dlgo.networks.alphago import alphago_model
 
 rows, cols = 19, 19
 num_classes = rows * cols
@@ -13,3 +14,8 @@ encoder = AlphaGoEncoder()
 processor = GoDataProcessor(encoder=encoder.name())
 generator = processor.load_go_data('train', num_games, use_generator=True)
 test_generator = processor.load_go_data('test', num_games, use_generator=True)
+
+input_shape = (encoder.num_planes, rows, cols)
+alphago_sl_policy = alphago_model(input_shape, is_policy_net=True)
+
+alphago_sl_policy.compile('sgd', 'categorical_crossentropy', metrics=['accuracy'])
