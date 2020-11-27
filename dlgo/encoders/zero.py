13ad41e4d9f83fb177131bbe5231fc12f2c46383
JISUNG PARK <llllllllll@kakao.com>
2020-11-28 07:39:12 +0900

    feat: Add encode

--- dlgo/encoders/zero.py
+++ dlgo/encoders/zero.py
@@ -2,8 +2,10 @@
 # <llllllllll@kakao.com>
 # MIT License
 
+import numpy as np
+
 from dlgo.goboard import Move
-from dlgo.gotypes import Point
+from dlgo.gotypes import Point, Player
 
 # 1. 활로가 1개인 자신의 돌
 # 2. 활로가 2개인 자신의 돌
@@ -38,3 +40,30 @@ class ZeroEncoder:
 
     def num_moves(self):
         return self.board_size * self.board_size + 1
+
+    def encode(self, game_state):
+        board_tensor = np.zeros(self.shape())
+        next_player = game_state.next_player
+        if game_state.next_player == Player.white:
+            board_tensor[8] = 1
+        else:
+            board_tensor[9] = 1
+        for r in range(self.board_size):
+            for c in range(self.board_size):
+                p = Point(row=r + 1, col=c + 1)
+                go_string = game_state.board.get_go_string(p)
+
+                if go_string is None:
+                    if game_state.does_move_violate_ko(next_player,
+                                                       Move.play(p)):
+                        board_tensor[10][r][c] = 1
+                else:
+                    liberty_plane = min(4, go_string.num_liberties) - 1
+                    if go_string.color != next_player:
+                        liberty_plane += 4
+                    board_tensor[liberty_plane][r][c] = 1
+
+        return board_tensor
+
+    def shape(self):
+        return self.num_planes, self.board_size, self.board_size
