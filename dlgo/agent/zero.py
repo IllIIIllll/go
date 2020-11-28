bc11c55e8f3d328497f2f71fbfb35461bac718b8
JISUNG PARK <llllllllll@kakao.com>
2020-11-28 14:32:21 +0900

    feat: Add Zero Tree

--- dlgo/agent/zero.py
+++ dlgo/agent/zero.py
@@ -7,3 +7,25 @@ class Branch:
         self.prior = prior
         self.visit_count = 0
         self.total_value = 0.0
+
+class ZeroTreeNode:
+    def __init__(self, state, value, priors, parent, last_move):
+        self.state = state
+        self.value = value
+        self.parent = parent
+        self.last_move = last_move
+        self.total_visit_count = 1
+        self.branches = {}
+        for move, p in priors.items():
+            if state.is_valid_move(move):
+                self.branches[move] = Branch(p)
+        self.children = {}
+
+    def moves(self):
+        return self.branches.keys()
+
+    def add_child(self, move, child_node):
+        self.children[move] = child_node
+
+    def has_child(self, move):
+        return move in self.children
