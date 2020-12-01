d5ee17734ee248a78cae40593016c4e4a771e6cd
JISUNG PARK <llllllllll@kakao.com>
2020-12-02 03:17:04 +0900

    test: Simulate game
    자체 대국 구현

---
+++ test_zero.py
@@ -0,0 +1,32 @@
+# © 2020 지성. all rights reserved.
+# <llllllllll@kakao.com>
+# MIT License
+
+from dlgo.goboard import GameState, Player
+from dlgo import scoring
+
+def simulate_game(
+        board_size,
+        black_agent, black_collector,
+        white_agent, white_collector):
+    print('Starting the game!')
+    game = GameState.new_game(board_size)
+    agents = {
+        Player.black: black_agent,
+        Player.white: white_agent,
+    }
+
+    black_collector.begin_episode()
+    white_collector.begin_episode()
+    while not game.is_over():
+        next_move = agents[game.next_player].select_move(game)
+        game = game.apply_move(next_move)
+
+    game_result = scoring.compute_game_result(game)
+    print(game_result)
+    if game_result.winner == Player.black:
+        black_collector.complete_episode(1)
+        white_collector.complete_episode(-1)
+    else:
+        black_collector.complete_episode(-1)
+        white_collector.complete_episode(1)
